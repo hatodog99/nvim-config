364be9aaf4f9a9ea6a1803090cf386e1cb67e783
hatodog99 <195627582+hatodog99@users.noreply.github.com>
2025-08-09 02:11:17 +0800

    Add files

--- init.lua
+++ init.lua
@@ -40,5 +40,5 @@ require('lazy').setup {
   require 'plugins.alpha',
   require 'plugins.indent-blankline',
   require 'plugins.misc',
-  require 'plugins.live-servert',
+  require 'plugins.live-server',
 }
