53db4df4615bb395c4ea1b2a607e9f7258c1902e
hatodog99 <195627582+hatodog99@users.noreply.github.com>
2025-08-09 02:04:34 +0800

    add files

---
+++ init.lua
@@ -0,0 +1,44 @@
+require 'core.options'
+require 'core.keymaps'
+
+vim.g.have_nerd_font = true
+
+-- LAZY SETUP
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+local rtp = vim.opt.rtp:prepend(lazypath)
+
+-- Import color theme based on environment variable NVIM_THEME
+local default_color_scheme = 'onedark'
+local env_var_nvim_theme = os.getenv 'NVIM_THEME' or default_color_scheme
+
+-- Define a table of theme modules
+local themes = {
+  nord = 'plugins.themes.nord',
+  onedark = 'plugins.themes.onedark',
+}
+
+-- PLUGINS
+require('lazy').setup {
+  require 'plugins.neotree',
+  require 'plugins.themes.onedark',
+  require 'plugins.bufferline',
+  require 'plugins.lualine',
+  require 'plugins.treesitter',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocompletion',
+  require 'plugins.none-ls',
+  require 'plugins.gitsigns',
+  require 'plugins.alpha',
+  require 'plugins.indent-blankline',
+  require 'plugins.misc',
+  require 'plugins.live-server',
+}
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,26 @@
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+return {
+  'lewis6991/gitsigns.nvim',
+
+  config = function()
+    require('gitsigns').setup {}
+
+    vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', { desc = '[G]it Preview [H]unk' })
+  end,
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    signs_staged = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+}
